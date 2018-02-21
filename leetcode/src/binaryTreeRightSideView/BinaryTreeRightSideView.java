4a3b786bd1548e944b1a62e6253c1ef3fb2baa57
Ning Liu <nliu@Nings-MacBook-Pro-2.local>
2018-02-21 11:49:26 -0800

    #199 Binary Tree Right Side View

---
+++ leetcode/src/binaryTreeRightSideView/BinaryTreeRightSideView.java
@@ -0,0 +1,48 @@
+package binaryTreeRightSideView;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+import utility.TreeNode;
+
+public class BinaryTreeRightSideView {
+    public List<Integer> rightSideView(TreeNode root) {
+        List<Integer> res = new ArrayList<>();
+        if (root == null) {
+            return res;
+        }
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            for (int i = 0; i < size; ++i) {
+                TreeNode node = queue.poll();
+                if (i == size - 1) {
+                    res.add(node.val);
+                }
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+        return res;
+    }
+    
+    public static void main(String[] args) {
+        TreeNode root = new TreeNode(5);
+        root.left = new TreeNode(3);
+        root.right = new TreeNode(4);
+        root.right.left = new TreeNode(1);
+        root.right.right = new TreeNode(2);
+
+        BinaryTreeRightSideView obj = new BinaryTreeRightSideView();
+        List<Integer> res = obj.rightSideView(root);
+        res.stream().forEach(System.out::println);
+    }
+}
