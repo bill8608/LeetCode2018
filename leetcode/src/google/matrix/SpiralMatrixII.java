4de88ad8f9ebb71e4ccd8f64d718e95a67bb58b2
nliu365 <nliu8608@gmail.com>
2018-03-01 02:50:33 -0800

    #59 Spiral Matrix II

--- leetcode/src/google/matrix/SpiralMatrixII.java
+++ leetcode/src/google/matrix/SpiralMatrixII.java
@@ -16,7 +16,7 @@ public class SpiralMatrixII {
         int val = 1;
         while (n > 0) {
             if (n == 1) {
-                matrix[n - 1][n - 1] = 1;
+                matrix[x][y] = val;
                 break;
             }
             for (int i = 0; i < n - 1; ++i) {
@@ -41,7 +41,7 @@ public class SpiralMatrixII {
     public static void main(String[] args) {
         SpiralMatrixII spiralMatrixII = new SpiralMatrixII();
         //int[][] matrix = spiralMatrixII.generateMatrix(0);
-        int[][] matrix = spiralMatrixII.generateMatrix(4);
+        int[][] matrix = spiralMatrixII.generateMatrix(5);
         for (int[] row : matrix) {
             for (int val : row) {
                 System.out.print(val + " ");
